d81afef131168cd531e239fc157da43d658e30a6
Macharia Marigi <macharia.marigi@andela.com>
2017-10-22 14:37:36 +0300

    [CHORE]Restructure folders and files

---
+++ src/containers/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from '../images/logo.svg';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        App Home
+      </div>
+    )
+  }
+}
+
+export default App;
