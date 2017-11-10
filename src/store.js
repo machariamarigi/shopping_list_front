88a651d663484c55f21dde5675cf29cf101bc356
Macharia Marigi <macharia.marigi@andela.com>
2017-11-10 12:58:06 +0300

    [CHORE]Set up redux store and root reducer

--- src/store.js
+++ src/store.js
@@ -1,36 +1,11 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import { routerMiddleware } from 'react-router-redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import createHistory from 'history/createBrowserHistory';
+import promise from 'redux-promise';
+import logger from 'redux-logger';
 
-import rootReducer from './modules';
+import rootReducer from './reducers';
 
-export const history = createHistory();
-
-const initialState = {};
-const enhancers = [];
-const middleware = [
-  thunk,
-  routerMiddleware(history)
-];
-
-if (process.env.NODE_ENV === 'development') {
-  const devToolsExtension = window.devToolsExtension
-
-  if (typeof devToolsExtension == 'function') {
-    enhancers.push(devToolsExtension());
-  }
-}
-
-const composedEnhancers = compose(
-  applyMiddleware(...middleware),
-  ...enhancers,
-);
-
-const store = createStore(
-  rootReducer,
-  initialState,
-  composedEnhancers,
-);
+const middleware = applyMiddleware(thunk, promise, logger);
+const store = createStore(rootReducer, middleware);
 
 export default store;
