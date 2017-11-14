8f022d8b85488fef8835b9f603560639996b3bcb
Macharia Marigi <macharia.marigi@andela.com>
2017-11-14 13:07:42 +0300

    [CHORE]Add some form validators

---
+++ src/helpers/formValidators.js
@@ -0,0 +1,13 @@
+export const required = value => (value ? undefined : 'Required');
+
+export const email = value =>
+  (value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)
+    ? 'Invalid email address'
+    : undefined);
+
+export const minLength = min => value =>
+  (value && value.length < min ? `Must be ${min} characters or more` : undefined);
+
+export const alphaNumeric = value =>
+  (value && /[^a-zA-Z0-9 ]/i.test(value) ? 'Only alphanumeric characters' : undefined);
+
