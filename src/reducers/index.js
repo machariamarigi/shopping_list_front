131a6109a5efa975d9535243e8214cd73d80bccd
Macharia Marigi <macharia.marigi@andela.com>
2017-11-15 10:21:25 +0300

    [FEATURE]Add login reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,9 +3,11 @@ import { reducer as formReducer } from 'redux-form';
 
 import registration from './registrationReducer';
 import alert from './alertReducer';
+import authentication from './loginReducer'
 
 export default combineReducers({
   form: formReducer,
   registration,
   alert,
+  authentication,
 });
