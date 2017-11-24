e17ce4864b262195723d645655bfe28ea9804fe6
Macharia Marigi <macharia.marigi@andela.com>
2017-11-24 10:06:03 +0300

    [Feature] add a get user action

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -3,16 +3,17 @@ import alertActions from './alertActions';
 import authService from '../services/authService';
 
 export function register(user, callback) {
-  const request = () => ({ type: types.REGISTER_REQUEST, user });
-  const success = () => ({ type: types.REGISTER_SUCCESS, user });
+  const request = () => ({ type: types.REGISTER_REQUEST });
+  const success = response => ({ type: types.REGISTER_SUCCESS, response });
   const failure = error => ({ type: types.REGISTER_FAILURE, error });
   return (dispach) => {
-    dispach(request(user));
+    dispach(request());
 
     authService.register(user).then(
-      () => {
-        dispach(success(user));
+      (response) => {
+        dispach(success(response));
         callback('/login');
+        dispach(alertActions.success(response.message));
       },
       (error) => {
         dispach(failure(error));
@@ -22,21 +23,42 @@ export function register(user, callback) {
   };
 }
 
-export function login({ email, password }) {
-  const request = user => ({ type: types.LOGIN_REQUEST, user });
-  const success = user => ({ type: types.LOGIN_SUCCESS, user });
+export function login({ email, password }, callback) {
+  const request = () => ({ type: types.LOGIN_REQUEST });
+  const success = response => ({ type: types.LOGIN_SUCCESS, response });
   const failure = error => ({ type: types.LOGIN_FAILURE, error });
   return (dispach) => {
     dispach(request(email));
 
     authService.login(email, password).then(
-      () => {
-        dispach(success(email));
+      (response) => {
+        dispach(success(response));
+        dispach(alertActions.success(response.message));
+        dispach(getUser())
+        callback('/dashboard');
       },
       (error) => {
         dispach(failure(error));
         dispach(alertActions.error(error.response.data.message));
+        localStorage.clear()
       },
     );
+  };
+}
+
+export function getUser() {
+  const request = () => ({ type: types.GET_USER_REQUEST });
+  const success = response => ({ type: types.GET_USER_SUCCESS, response });
+  const failure = error => ({ type: types.GET_USER_FAILURE, error });
+
+  return (dispach) => {
+    dispach(request());
+
+    authService.getUser().then((response) => {
+      dispach(success(response));
+    }, (error) => {
+      dispach(failure(error));
+      dispach(alertActions.error(error.response.data.message));
+    });
   };
 }
