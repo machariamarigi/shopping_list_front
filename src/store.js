572f7621151096a0b86d11cd7556a4f6082df581
Macharia Marigi <macharia.marigi@andela.com>
2017-11-24 10:14:18 +0300

    [Chore] Compose reducer devtools

--- src/store.js
+++ src/store.js
@@ -2,10 +2,19 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import promise from 'redux-promise';
 import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import rootReducer from './reducers';
+import { getUser } from './actions/authActions';
 
-const middleware = applyMiddleware(thunk, promise, logger);
-const store = createStore(rootReducer, middleware);
+const middleware = applyMiddleware(thunk, logger);
 
-export default store;
+const configureStore = () => {
+  const store = createStore(rootReducer, composeWithDevTools(middleware));
+
+  store.dispatch(getUser());
+
+  return store;
+};
+
+export default configureStore;
