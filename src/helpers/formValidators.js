6571e33fd1be1ed346d2b1881598e79e91ab935f
Macharia Marigi <macharia.marigi@andela.com>
2017-11-27 12:39:25 +0300

    [Feature #153129693] Add an Item form

--- src/helpers/formValidators.js
+++ src/helpers/formValidators.js
@@ -11,3 +11,5 @@ export const minLength = min => value =>
 export const alphaNumeric = value =>
   (value && /[^a-zA-Z0-9 ]/i.test(value) ? 'Only alphanumeric characters' : undefined);
 
+export const numericOnly = value =>
+  (value && /[^0-9 ]/i.test(value) ? 'Only numeric characters' : undefined);
