5eac000a87a6d09ab9bb0f111cc201055860360c
Macharia Marigi <macharia.marigi@andela.com>
2017-12-19 12:42:01 +0300

    [Update] Use the online API

--- src/helpers/axiosConfig.js
+++ src/helpers/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosConfig = axios.create({
-  baseURL: 'http://127.0.0.1:5000/api/v1/',
+  baseURL: 'https://shopping-listy-api.herokuapp.com/api/v1/',
   timeout: 15000,
   headers: {
     'Content-Type': 'application/json',
