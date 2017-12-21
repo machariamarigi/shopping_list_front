3ca35f10d7102c322977f24b4b67085a27ce1b2e
Macharia Marigi <macharia.marigi@andela.com>
2017-12-21 12:23:09 +0300

    [Chore #153129797] Improve the 404 page

--- src/components/UI/NotFound.js
+++ src/components/UI/NotFound.js
@@ -1,5 +1,19 @@
 import React from 'react';
+import { Card, CardActions, CardHeader, CardText } from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
 
-const notFound = () => <div className="error-page">404</div>;
+const notFound = () => (
+  <div className="error-page">
+    <Card className="not-found">
+      <CardHeader title="404" subtitle="Page Not Found :(" />
+      <CardText>
+        Maybe the page you are looking for has been removed, or you typed in the wrong URL
+      </CardText>
+      <CardActions>
+        <FlatButton label="Go to homepage" primary href="/dashboard" />
+      </CardActions>
+    </Card>
+  </div>
+);
 
 export default notFound;
